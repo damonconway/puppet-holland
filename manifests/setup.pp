83df18edcd54a3edb55f886b35c24e66dec18f90
Damon Conway <damon.conway@alchemysystems.com>
2018-03-29 15:32:11 -0700

    Adding holland.conf management.

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,10 +1,15 @@
 # holland::setup
 #
-# A description of what this class does
+# Setup config files and anything else that all backupsets will need or have access to.
 #
-# @summary A short summary of the purpose of this class
+# @summary General holland setup
 #
-# @example
-#   include holland::setup
 class holland::setup {
+
+  $config_file = $holland::config_file
+  $config_options = $holland::_config_options
+  $defaults = { 'path' => $config_file }
+
+  create_ini_settings($config_options, $defaults)
+
 }
