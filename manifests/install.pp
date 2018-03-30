d5e44df8eb00198960ae0a081d474a977762d08e
Damon Conway <damon.conway@alchemysystems.com>
2018-03-30 12:57:23 -0700

    Adding debugging messages.

--- manifests/install.pp
+++ manifests/install.pp
@@ -25,17 +25,24 @@ class holland::install {
   package { "${package_prefix}common": }
 
   if $modules {
+    debug { "holland::modules was set.": }
     if $modules.is_a(String) {
+      debug { "holland::modules is a String.": }
       $mod_name = "${package_prefix}${modules}"
+      debug { "holland::install::mod_name = $mod_name": }
       package { $mod_name: }
     } elsif $modules.is_a(Array) {
+      debug { "holland::modules is an Array.": }
       $modules.each |$mod| {
         $mod_name = "${package_prefix}${mod}"
+        debug { "holland::install::mod_name = $mod_name": }
         package { $mod_name: }
       }
     } elsif $modules.is_a(Hash) {
+      debug { "holland::modules is a Hash.": }
       $modules.each |$mod,$opts| {
         $mod_name = "${package_prefix}${mod}"
+        debug { "holland::install::mod_name = $mod_name": }
         package { $mod_name: * => $opts }
       }
     }
